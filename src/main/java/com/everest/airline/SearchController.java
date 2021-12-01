3cc06cbd79db52f013ef473d12c748c2739bc265
Charan <sai.peram@everest.engineering>
2021-12-01 14:48:55 +0530

    add search flights feature(without departure date)

--- src/main/java/com/everest/airline/SearchController.java
+++ src/main/java/com/everest/airline/SearchController.java
@@ -4,6 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 @Controller
 public class SearchController {
 
@@ -14,7 +20,16 @@ public class SearchController {
 
     @RequestMapping(value = "/search")
     public String search(String from, String to, Model model) {
-        model.addAttribute("flights", Data.flights);
+        List<Flight> flights = searchFlights(from, to, Data.flights);
+        model.addAttribute("flights", flights);
         return "search";
+    }
+
+    private List<Flight> searchFlights(String origin, String destination, List<Flight> flights){
+        List<Flight> flightStream = flights.stream()
+                .filter(flight -> (Objects.equals(flight.getSource(), origin) && Objects.equals(flight.getDestination(), destination)))
+                .collect(Collectors.toList());
+        System.out.println(flightStream);
+        return flightStream;
     }
 }
